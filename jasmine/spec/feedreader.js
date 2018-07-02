fdbfb1ab6461be156020eecb9051fc3e5bd8733e
Julio Venero Loayza <venerojulio@gmail.com>
2018-07-02 16:07:49 -0500

    test: names are defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -43,6 +43,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have name defined', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+         });
     });
 
 
